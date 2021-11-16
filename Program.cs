d68b0cd5e12752170d04b8d64b719959b2f1c548
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-11-16 12:31:57 +0700

    Insert

--- Program.cs
+++ Program.cs
@@ -35,10 +35,38 @@ namespace EntityFramework
                 System.Console.WriteLine($"{dbName} was not deleted.");
             }
         }
+        static void InsertProduct()
+        {
+            using var dbContext = new ProductDbContext();
+            // var p1 = new Product();
+            // p1.ProductName = "Product 1";
+            // p1.Provider = "Company 1";
+            // dbContext.Add(p1);
+
+            // var p2 = new Product()
+            // {
+            //     ProductName = "Product 2",
+            //     Provider = "Company 2"
+            // };
+            // dbContext.Add(p2);
+
+            var products = new object[] {
+                new Product() { ProductName = "Product 3", Provider = "Company 3" },
+                new Product() { ProductName = "Product 4", Provider = "Company 4" },
+                new Product() { ProductName = "Product 5", Provider = "Company 5" },
+            };
+            dbContext.AddRange(products);
+
+            // Save
+            int numberRows = dbContext.SaveChanges();
+            System.Console.WriteLine($"Inserted {numberRows} rows");
+        }
         static void Main(string[] args)
         {
-            CreateDatabase();
+            // CreateDatabase();
             // DropDatabase();
+
+            InsertProduct();
         }
     }
 }
