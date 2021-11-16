44a79b2fb34d1d60b5bd6a7f5bbbb562972fd486
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-11-16 16:14:38 +0700

    Logger

--- Models/ProductDbContext.cs
+++ Models/ProductDbContext.cs
@@ -1,14 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EntityFramework
 {
     public class ProductDbContext : DbContext
     {
+        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddFilter(DbLoggerCategory.Query.Name, LogLevel.Information);
+            builder.AddConsole();
+        });
+
         public DbSet<Product> products { get; set; }
         private const string connectionString = "Server=TUONG\\SQLEXPRESS;Database=data01;Trusted_Connection=True;";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseLoggerFactory(loggerFactory);
             optionsBuilder.UseSqlServer(connectionString);
         }
     }
