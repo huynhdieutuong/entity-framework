c54d521c74a8506cf68b3979addc279e16098bab
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-11-21 14:34:07 +0700

    Migration: Basic

--- Program.cs
+++ Program.cs
@@ -9,8 +9,34 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
-            using var dbContext = new WebContext();
-            dbContext.Database.EnsureCreated();
+            // 0. Install dotnet-ef: dotnet tool install --global dotnet-ef
+
+            // Migration (create a snapshot for database)
+
+            /*
+            1. Create migration: 
+                `````` dotnet ef migrations add MigrationName
+                => automatically create Migrations folder
+            
+            dotnet ef migrations add V0
+            dotnet ef migrations add V1 (if code V1 in Models not changed, 20211121070844_V1 is nothing)
+
+            2. Check list & status migrations:
+                `````` dotnet ef migrations list
+                => 20211121065741_V0 (Pending) => V0: MigrationName | Pending: not yet updated to database
+
+            3. Remove the latest migration:
+                `````` dotnet ef migrations remove
+                => Remove migration V1
+
+            4. Update migrations to database:
+                `````` dotnet ef database update [MigrationName]
+                => Create database & tables (if not MigrationName, select the latest migrations)
+
+            5. Delete database:
+                `````` dotnet ef database drop -f
+                => Drop database
+            */
         }
     }
 }
